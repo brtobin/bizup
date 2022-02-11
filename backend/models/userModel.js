3ff2c8da9c698d14bd0da3a91c7b06c77bf82921
Chirag-Bhardwaj <chiragbhardwaj2002@gmail.com>
2022-02-11 17:49:17 -0600

    Add user data model

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    }
+});
+const User = mongoose.model("User", UserSchema);
+
+/* -------------------------------------------------------------------------- */
+
+module.exports = User;
