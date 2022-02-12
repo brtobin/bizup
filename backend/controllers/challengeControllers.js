e71f13939c2a73305f19c67c6e28ce9f21c32c14
Max <80323354+GuyBritish@users.noreply.github.com>
2022-02-12 06:16:45 -0600

    Add update user and business controllers

--- backend/controllers/challengeControllers.js
+++ backend/controllers/challengeControllers.js
@@ -1,9 +1,35 @@
+const mongoose = require("mongoose");
+
+const Business = require("../models/businessModel");
 const Challenge = require("../models/challengeModel");
+const User = require("../models/userModel");
+
+/* -------------------------------------------------------------------------- */
+
+const updateBusiness = async (id, challengeId) => {
+	let business = await Business.findById(id);
+	if (business.challenges.includes(id)) {
+		business.challenges = business.challenges.filter((chall) => {
+			return chall !== challengeId;
+		});
+	} else {
+		business.challenges.push(id);
+	}
+	await business.save();
+};
+
+const updateUsers = async (userId, challengeId, coin) => {
+	let user = await User.findById(userId);
+	user.achievements.push(challengeId);
+	user.coins += coin;
+	const newUser = await user.save();
+	return newUser;
+};
 
 /* -------------------------------------------------------------------------- */
 
 const addChallenge = async (req, res) => {
-	const { title, description, icon, startTime, expirationTime, category } = req.body;
+	const { title, description, icon, startTime, expirationTime, category, reward } = req.body;
 
 	const challenge = await Challenge.findOne({ title: title, host: req.business._id });
 
@@ -18,10 +44,12 @@ const addChallenge = async (req, res) => {
 		startTime,
 		expirationTime,
 		category,
+		reward,
 		host: req.business._id,
 	});
 
 	if (newChallenge) {
+		updateBusiness(newChallenge.host, newChallenge._id);
 		res.status(201).json({
 			_id: newChallenge._id,
 			title: newChallenge.title,
@@ -29,6 +57,7 @@ const addChallenge = async (req, res) => {
 			icon: newChallenge.icon,
 			startTime: newChallenge.startTime,
 			expirationTime: newChallenge.expirationTime,
+			reward: newChallenge.reward,
 			host: newChallenge.host,
 		});
 	} else {
@@ -62,6 +91,7 @@ const getChallenge = async (req, res) => {
 			icon: challenge.icon,
 			startTime: challenge.startTime,
 			expirationTime: challenge.expirationTime,
+			reward: challenge.reward,
 			host: challenge.host,
 		});
 	} else {
@@ -80,6 +110,7 @@ const updateChallenge = async (req, res) => {
 		challenge.icon = req.body.icon || challenge.icon;
 		challenge.startTime = req.body.startTime || challenge.startTime;
 		challenge.expirationTime = req.body.expirationTime || challenge.expirationTime;
+		challenge.reward = req.body.reward || challenge.reward;
 
 		const updatedChallenge = await business.save();
 
@@ -90,6 +121,7 @@ const updateChallenge = async (req, res) => {
 			icon: updatedChallenge.icon,
 			startTime: updatedChallenge.startTime,
 			expirationTime: updatedChallenge.expirationTime,
+			reward: updatedChallenge.reward,
 			host: updatedChallenge.host,
 		});
 	} else {
@@ -102,6 +134,7 @@ const deleteChallenge = async () => {
 	const params = req.params;
 	const removedChallenge = await Challenge.deleteOne({ _id: params.id });
 	if (removedChallenge) {
+		updateBusiness(removedChallenge.host, removedChallenge._id);
 		res.json({
 			_id: removedChallenge._id,
 			title: removedChallenge.title,
@@ -109,6 +142,7 @@ const deleteChallenge = async () => {
 			icon: removedChallenge.icon,
 			startTime: removedChallenge.startTime,
 			expirationTime: removedChallenge.expirationTime,
+			reward: removedChallenge.reward,
 			host: removedChallenge.host,
 		});
 	} else {
@@ -117,6 +151,32 @@ const deleteChallenge = async () => {
 	}
 };
 
+const awardUser = async (req, res) => {
+	const params = req.params;
+	const winningUser = await User.findOne({ email: params.user });
+	let challenge = await Challenge.findById(params.id);
+	if (challenge && winningUser) {
+		if (challenge.completedUsers.includes(winningUser._id)) {
+			res.status(401);
+			throw new Error("User has already won this challenge");
+		}
+		challenge.completedUsers.push(winningUser._id);
+		const newUser = await updateUsers(winningUser._id, challenge._id, challenge.reward);
+		const newChallenge = await challenge.save();
+		res.json(newUser);
+	} else {
+		res.status(404);
+		throw new Error("Challenge or user not found");
+	}
+};
+
 /* -------------------------------------------------------------------------- */
 
-module.exports = { addChallenge, getChallenge, updateChallenge, deleteChallenge, getAllChallenges };
+module.exports = {
+	addChallenge,
+	getChallenge,
+	updateChallenge,
+	deleteChallenge,
+	getAllChallenges,
+	awardUser,
+};
