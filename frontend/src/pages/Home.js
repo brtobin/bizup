a9dc585f71e24780f0de1b6387bb23e615e678b0
raceice19 <56613698+raceice19@users.noreply.github.com>
2022-02-12 02:58:59 -0600

    Fixed Task layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,47 @@
-import React, {useState} from 'react'
+import React from 'react'
+import challengeItem from '../components/ChallengeItem/challengeItem'
 import './Home.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+
+
 const Home = () => {
-  
+  let info = {
+    icon: './../images/icons8-address-book-48.png',
+    title: 'This is challenge 2',
+    host: 'Target',
+    reward: 1000,
+    expDate: '10/19/2022'
+  }
+
   let tasks = [];
-  for (let i=0; i<5; i++){
+  for (let i=0; i<100; i++){
     tasks.push({
       complete: 0,
-      task: `There is no task ${i}`,
-      points: 0
+      task: challengeItem(info)
     })
+  }
+
+  function checkComplete() {
+    console.log('true');  
   }
 
 
   return(
 
     <div className='Home'> 
-      {tasks.map( (task, index) => (
-      <p key={index}>
-      <input type='checkbox' id={"task"+index}></input>
-      <label htmlFor={"task"+index}> {task.task} </label>
-      </p>
-      ))}  
+      <Container overflow='scroll'>
+        {tasks.map( (task, index) => (
+        <div key={index}>
+          <input type='checkbox' className='Checkbox' id={"task"+index}  onClick={checkComplete}></input>
+          <label htmlFor={"task"+index}> {task.task} </label>
+        </div>
+
+        ))}
+      </Container>
     </div>
+  
+   
  
   );
 }
