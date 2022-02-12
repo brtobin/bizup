c6af12431b95530e2c83518a961580426fdcc337
raceice19 <56613698+raceice19@users.noreply.github.com>
2022-02-11 21:09:56 -0600

    Basic checkboxes added
    added checkboxes with text

---
+++ frontend/src/components/Feed/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feed.css";
+
+const Feed = () => {
+    return (
+        <div className="feed">
+            <input type='checkbox' id='task'></input>
+            <label htmlFor='task'>This is the task</label>
+        </div>
+        
+    );
+};
+
+
+export default Feed;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import logo from '../logo.svg';
+import Feed from '../components/Feed/Feed';
 
+class Task extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      complete: 0,
+      task: 'No task given'
+    }
+    
+
+  }
+}
 const Home = () => {
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Feed'>
+          <Feed/>
+          <Feed/>
+          <Feed/>
+          <Feed/>
+          <Feed/>
+        </div>
       </header>
     </div>
     )
