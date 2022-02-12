b3ec3a57031668bccc0ffa86a0a5cd6b867ccb92
raceice19 <56613698+raceice19@users.noreply.github.com>
2022-02-12 11:20:09 -0600

    Extra data

--- backend/data/businesses.js
+++ backend/data/businesses.js
@@ -10,6 +10,33 @@ const businesses = [
 		address: "xy, Lake st., Madison, Wisconsin",
 		description: "Manufacturers of xy devices",
 	},
+	{
+		type: "business",
+		name: "XY Enterprises",
+		email: "xy@progamer.com",
+		password: bcrypt.hashSync("123456", 11),
+		image: "https://en.wikipedia.org/wiki/.google#/media/File:Google_2015_logo.svg",
+		address: "xy, Lake st., Madison, Wisconsin",
+		description: "Manufacturers of xy devices",
+	},
+	{
+		type: "restuarant",
+		name: "Kiki's Delivery Service",
+		email: "kiki@delivery.com",
+		password: bcrypt.hashSync("987484", 11),
+		image: "",
+		address: "xy, Lake st., Madison, Wisconsin",
+		description: "Has good food on a broom",
+	},
+	{
+		type: "business",
+		name: "Fence-mart",
+		email: "fence@mart.com",
+		password: bcrypt.hashSync("333666", 11),
+		image: "",
+		address: "xy, Lake st., Madison, Wisconsin",
+		description: "Has good food on a broom",
+	},
 ];
 /* -------------------------------------------------------------------------- */
 module.exports = businesses;
