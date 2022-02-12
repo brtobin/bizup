a51f11871d7a7de0de4cfbb19a6ec015d11d3463
brtobin <btobin86@gmail.com>
2022-02-11 19:28:29 -0600

    Add navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <h2>Bizzup</h2>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
