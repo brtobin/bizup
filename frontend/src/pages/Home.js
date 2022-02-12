1d7276cfe9018da39d7f7d6c8f6be7cc3a469e69
raceice19 <56613698+raceice19@users.noreply.github.com>
2022-02-12 00:12:07 -0600

    Updated Feed
    Added multiple tasks

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react'
 import Feed from '../components/Feed/Feed';
 
-const Home = () => {
-    return (
+class Home extends React.Component {
+
+  render() {  
+  return (
     <div className="App">
       <header className="App-header">
         <Feed/>
-
       </header>
     </div>
     )
+  }
 }
 
 export default Home
